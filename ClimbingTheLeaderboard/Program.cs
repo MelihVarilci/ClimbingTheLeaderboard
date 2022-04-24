b1abad3651e66b6f6a859412c035a9e9f67e6380
MelihVarilci <varilci.melih@gmail.com>
2022-04-24 15:24:31 +0300

    Performance Issue Resolved With Binary Search

--- ClimbingTheLeaderboard/Program.cs
+++ ClimbingTheLeaderboard/Program.cs
@@ -28,7 +28,6 @@ List<int> rankeds = new()
     20,
     10,
     9,
-    8,
     7,
     6,
     5,
@@ -37,19 +36,20 @@ List<int> rankeds = new()
 List<int> players = new()
 {
     5,
+    8,
     25,
     50,
     100,
     120
 };
 
-static List<long> largeInputFileTransfer()
+static long[] largeInputFileTransfer()
 {
     var inputTxt = File.ReadAllText(@"C:\Users\Dell5501-1\source\repos\ClimbingTheLeaderboard\ClimbingTheLeaderboard\input06.txt");
     var splitInputTxt = Regex.Replace(inputTxt, @"\t|\n|\r", "")
         .Split(" ").ToList();
 
-    return new HashSet<long>(splitInputTxt.ConvertAll(s => Int64.Parse(s))).OrderBy(x => x).ToList();
+    return new HashSet<long>(splitInputTxt.ConvertAll(s => Int64.Parse(s))).OrderBy(x => x).ToArray();
 }
 
 static List<long> largeOutputFileTransfer()
@@ -61,16 +61,48 @@ static List<long> largeOutputFileTransfer()
     return new HashSet<long>(splitOutputTxt.ConvertAll(s => Int64.Parse(s))).ToList();
 }
 
+static int binarySearch(long[] arr, long x)
+{
+    int l = 0, r = arr.Length - 1;
+    while (l <= r)
+    {
+        int m = l + (r - l) / 2;
+
+        // Check if x is present at mid
+        if (arr[m] == x)
+            return m;
+
+        // If x greater, ignore left half
+        if (arr[m] < x)
+            r = m - 1;
+
+        // If x is smaller, ignore right half
+        else
+            l = m + 1;
+        //r = m - 1;
+    }
+
+    // if we reach here, then element was
+    // not present
+    return r + 1;
+}
+
 static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
 {
     // Terminated due to timeout :(
-    var rankedList = largeInputFileTransfer();
+    var rankedArray = largeInputFileTransfer();
     var playerList = largeOutputFileTransfer();
 
     // Congratulations!
     //var rankedList = new HashSet<int>(ranked).OrderBy(x => x).Reverse().ToArray();
 
-    return playerList.Select(x => rankedList.Count(y => y > x) + 1).ToList();
+    // Linear Search
+    //return playerList.Select(x => rankedList.Count(y => y > x) + 1).ToList();
+
+    // Binary Search
+    return playerList.Select(x => binarySearch(rankedArray, x))
+        .Select(y => y + 1)
+        .ToList();
 }
 
 Stopwatch stopwatch = new Stopwatch();
